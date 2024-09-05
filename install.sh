2f9bb91ece69acb7308cbe3e9a2f8356a67fd3ea
sevu11 <johan.sorman@icloud.com>
2024-09-06 00:05:18 +0200

    Adding a finish confirmation

--- install.sh
+++ install.sh
@@ -684,4 +684,11 @@ sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: A
 # Add sudo rights
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+echo -ne "
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+-------------------------------------------------------------------------
+                               Finished! 
+-------------------------------------------------------------------------
+"
 EOF
